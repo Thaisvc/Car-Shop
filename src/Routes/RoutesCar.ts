89fff500095a05a725f29648192160bca1fc8418
thais viana <thaisviana081@outlook.com>
2023-01-16 19:45:09 -0300

    ajuste routes

--- src/Routes/RoutesCar.ts
+++ src/Routes/RoutesCar.ts
@@ -4,22 +4,22 @@ import ControllerCar from '../Controllers/CarController';
 const routes = Router();
 
 routes.post(
-  '/cars',
+  '/',
   (req, res, next) => new ControllerCar(req, res, next).CreateCar(),
 );
 
 routes.get(
-  '/cars',
+  '/',
   (req, res, next) => new ControllerCar(req, res, next).findCar(),
 );
 
 routes.get(
-  '/cars/:id',
+  '/:id',
   (req, res, next) => new ControllerCar(req, res, next).idFindCar(),
 );
 
 routes.put(
-  '/cars/:id',
+  '/:id',
   (req, res, next) => new ControllerCar(req, res, next).updateCar(),
 );
 
